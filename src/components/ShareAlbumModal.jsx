9d39604e39ec63ed889783259c60d296bad0422e
Costantino-Scardigno <costantino98@icloud.com>
2025-03-13 15:59:19 +0100

    coomit 3

--- src/components/ShareAlbumModal.jsx
+++ src/components/ShareAlbumModal.jsx
@@ -30,7 +30,7 @@ const ShareAlbumModal = ({
     try {
       const token = localStorage.getItem("authToken");
       const response = await fetch(
-        `https://scattifestosi.netlify.app/api/events/${selectedAlbum.id}/share`,
+        `http://localhost:8080/api/events/${selectedAlbum.id}/share`,
         {
           method: "POST",
           headers: {
